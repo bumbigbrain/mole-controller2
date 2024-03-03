2f24cf9995e04fad0a3b2308e480a4c533e8fae6
wen <owen0813715430@hotmail.com>
2024-03-04 05:10:57 +0700

    100%

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,24 @@
 #include <ESP32Servo.h>
 #include <esp_now.h>
 #include <WiFi.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
 #define SERVO_PIN 13
 #define PUSH_BUTTON 19
 #define LDR_PIN 36
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
 
-uint8_t Controller1Address[] = {0x3C, 0x61, 0x05, 0x03, 0xA2, 0x74};
-esp_now_peer_info_t peerInfoController1;
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+
+
+
+
+uint8_t Controller2Address[] = {0x3C, 0x61, 0x05, 0x03, 0xD4, 0xB0};
+esp_now_peer_info_t peerInfoController2;
 
 
 typedef struct MoleMessage {
@@ -17,9 +28,71 @@ typedef struct MoleMessage {
 
 
 
+
+void displayNoMole() { 
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(23, 10);
+  display.println("No Mole");
+  display.display();
+}
+
+
+void displayMode0() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 10);
+  display.printf("GIGI MOLE");
+  display.setCursor(10, 20);
+  display.printf("1 HP");
+  display.display();
+}
+
+void displayMode1() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 10);
+  display.printf("SUPER GIGI MOLE"); 
+  display.setCursor(10, 20);
+  display.printf("1 HP");
+  display.setCursor(10, 30);
+  display.printf("1 GUARD");
+  display.display();
+}
+
+void displayMode2() { 
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 20);
+  display.printf("TANK MOLE");
+  display.setCursor(10, 30);
+  display.printf("3 HP");
+  display.display();
+}
+
+
+void displayMode3() {
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(15, 20);
+  display.printf("BOSS MOLE");
+  display.setCursor(10, 30);
+  display.printf("3 HP");
+  display.setCursor(10, 40);
+  display.printf("3 GUARD");
+  display.display();
+}
+
+
+
 MoleMessage MoleInfo;
 Servo myServo;
-
+int hp;
 
 
 
@@ -60,10 +133,25 @@ void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
   //   upTarget();
   // }
   memcpy(&MoleInfo, incomingData, sizeof(MoleInfo));
-  if (MoleInfo.mole == 1) {
-    UpMole();
+  if (MoleInfo.mole == 0) {
+    displayMode0();
   }
-  
+
+  if (MoleInfo.mole == 1) {
+    displayMode1(); 
+  }
+
+  if (MoleInfo.mole == 2) {
+    hp = 3; 
+    displayMode2();
+  }
+
+  if (MoleInfo.mole == 3) {
+    hp = 3; 
+    displayMode3();
+  }
+
+  UpMole();
 }
 
 
@@ -74,6 +162,14 @@ void setup() {
 
   Serial.begin(9600); 
   WiFi.mode(WIFI_STA);
+
+
+ //setup DISPLAY 
+  if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;);
+  }
+
 
   // init ESP-NOW
   if (esp_now_init() != ESP_OK) {
@@ -88,10 +184,10 @@ void setup() {
   esp_now_register_recv_cb(OnDataRecv);
 
   // register peer Controller1
-  memcpy(peerInfoController1.peer_addr, Controller1Address, 6);
-  peerInfoController1.channel = 0;
-  peerInfoController1.encrypt = false;
-  if (esp_now_add_peer(&peerInfoController1) != ESP_OK) {
+  memcpy(peerInfoController2.peer_addr, Controller2Address, 6);
+  peerInfoController2.channel = 0;
+  peerInfoController2.encrypt = false;
+  if (esp_now_add_peer(&peerInfoController2) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
   }
@@ -99,6 +195,7 @@ void setup() {
   //setup SERVO
   myServo.attach(SERVO_PIN);
   myServo.write(0);
+  MoleInfo.mole = -1;
 }
 
 
@@ -106,14 +203,60 @@ void setup() {
 
 
 void loop() {
-  if (Debounce() && MoleInfo.mole) { // the botton is pressed
-    // if MOLE UP do this 
-    esp_err_t result = esp_now_send(Controller1Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
-    MoleInfo.mole = 0;
-  }  
-  
-  if (MoleInfo.mole == 0) {
+
+
+  if (MoleInfo.mole == -1) {
+    displayNoMole();
     DownMole();
+  } 
+  
+  if (MoleInfo.mole == 0) { // 1 HP mole
+    if (Debounce() && analogRead(LDR_PIN) < 3000) { 
+      // isMoleBonked = true;
+      esp_err_t result = esp_now_send(Controller2Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
+      DownMole();
+      MoleInfo.mole = -1;
+    } else {
+      // isBonked = false;
+      //esp_err_t result = esp_now_send(Controller1Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
+    }
+    
+  } 
+  
+  if (MoleInfo.mole == 1) {
+    if (Debounce() && analogRead(LDR_PIN) > 3000) {
+      // isMoleBonked = true;
+      esp_err_t result = esp_now_send(Controller2Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
+      DownMole();
+      MoleInfo.mole = -1;
+    } else {
+      // isBonked = false;
+    }
+  }
+
+  
+  if (MoleInfo.mole == 2) { 
+    if (Debounce() && analogRead(LDR_PIN) < 3000) {
+      hp--;
+    } 
+    if (hp <= 0) { // don't come this again 
+      esp_err_t result = esp_now_send(Controller2Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
+      DownMole();
+      MoleInfo.mole = -1;
+    }     
+  }
+
+  
+  if (MoleInfo.mole == 3) { 
+    if (Debounce() && analogRead(LDR_PIN) > 3000) {
+      hp--;
+    } 
+    if (hp <= 0) {  // don't come this again
+      esp_err_t result = esp_now_send(Controller2Address, (uint8_t *) &MoleInfo, sizeof(MoleInfo)); 
+      DownMole();
+      MoleInfo.mole = -1;
+    }
+     
   }
 
 }
